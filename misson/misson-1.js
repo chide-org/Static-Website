125a7e60c728f746252d2bbff6c285549a65f06b
Rapheal <144027281+chide-org@users.noreply.github.com>
2024-01-15 00:04:23 +0800

    长期任务记录板

---
+++ misson/misson-1.js
@@ -0,0 +1,86 @@
+//获得总任务表元素并展示缓存数据
+let ul = document.querySelector("ul");
+console.log(localStorage.getItem("mission"));
+if (localStorage.getItem("mission") === "") {
+  localStorage.setItem(
+    "mission",
+    '<li data-piece="10" data-progress="1" style="--width: 9.5%;">示例（删除后添加您的任务）：10个分段<button type="button"> 提交</button><button type="button">删除</button></li>'
+  );
+} else {
+}
+ul.innerHTML = localStorage.getItem("mission");
+//监视新增按钮
+let addButton = document.getElementById("add");
+addButton.addEventListener("click", add);
+
+//定义新增函数
+function add() {
+  let newLi = document.createElement("li");
+  let nam = document.getElementById("name");
+  let num = document.getElementById("num");
+
+  ////输入合法性判断
+  if (num.value === "" || nam.value === "") {
+    return;
+  } else if (Number(num.value) < 1) {
+    alert("分段数量必须为正整数");
+    num.value = "";
+    return;
+  }
+
+  newLi.innerHTML =
+    nam.value +
+    '<button  type="button"> 提交</button><button type="button">删除</button>';
+  newLi.setAttribute("data-piece", num.value);
+  newLi.setAttribute("data-progress", 0);
+  ul.appendChild(newLi);
+  num.value = "";
+  nam.value = "";
+  //提示添加成功
+  console.log(
+    ////渐退出提示框
+    "新增任务成功！\n" +
+      "\n" +
+      newLi.innerText.slice(0, -2) +
+      "\n" +
+      newLi.dataset.piece
+  );
+  localStorage.setItem("mission", ul.innerHTML);
+}
+
+//监视提交和删除按钮
+ul.addEventListener("click", function (event) {
+  console.log("catched");
+
+  if (event.target.innerText === "删除") {
+    console.log("删除任务"); ////
+    let judge = window.confirm("确认删除这项任务吗？");
+    if (judge) {
+      ul.removeChild(event.target.parentNode);
+      localStorage.setItem("mission", ul.innerHTML);
+    }
+    return;
+  }
+
+  let uploadLi = event.target.parentNode;
+  let pie = Number(uploadLi.dataset.piece);
+  let pro = Number(uploadLi.dataset.progress);
+  console.log(pie);
+  console.log(pro);
+
+  if (pie === pro) {
+    console.log("这个任务已经完成啦\n 试试添加新的任务~~"); ////渐退出提示框
+    return;
+  }
+
+  let piece = (95 / pie) * (1 + pro);
+  uploadLi.style.setProperty("--width", piece + "%");
+  uploadLi.setAttribute("data-progress", 1 + Number(pro));
+
+  console.log(uploadLi.dataset.piece);
+  console.log(uploadLi.dataset.progress);
+  if (uploadLi.dataset.piece === uploadLi.dataset.progress) {
+    console.log("完成任务了呢！庆祝一下吧~~"); ////渐退出提示框
+  }
+  localStorage.setItem("mission", ul.innerHTML);
+});
